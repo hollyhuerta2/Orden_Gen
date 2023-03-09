9a6171c7c6915e846d639715a51d2a02c8ead918
Holly <holly.cg@outlook.com>
2023-03-09 01:51:51 -0600

    new commit

---
+++ src/main/java/org/example/Main1.java
@@ -0,0 +1,36 @@
+package org.example;
+
+    public class Main1 {
+
+        public static void main (String [] args) {
+            Circle circle1 = new Circle (10, 10, 20);
+            Circle circle2 = new Circle (15, 15, 21);
+
+            int resultCircles = circle1.compareTo (circle2);
+            System.out.println(resultCircles);
+           /* if(resultCircles<0){
+                System.out.println("circulo 1 es menor que el 2: "+resultCircles);
+            }else if(resultCircles>0){
+                System.out.println("circulo 1 es mayor que el 2:"+resultCircles);
+            }else{
+                System.out.println("son iguales: "+resultCircles);
+            }*/
+
+            Rectangle rec1 = new Rectangle (10, 10, 20, 20);
+            Rectangle rec2 = new Rectangle (30, 30, 50, 50);
+
+            int resultRecs = rec1.compareTo(rec2);
+            System.out.println(resultRecs);
+            if(resultRecs<0){
+                System.out.println("rectangulo 1 es menor que el 2: "+resultRecs);
+            }else if(resultRecs>0){
+                System.out.println("rectangulo 1 es mayor que el 2:"+resultRecs);
+            }else{
+                System.out.println("son iguales: "+resultRecs);
+            }
+
+
+        }
+
+    }
+
