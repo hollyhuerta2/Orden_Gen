c607192304b9ce22d9fdac041c2f33ed2a999878
Holly <holly.cg@outlook.com>
2023-05-05 23:30:26 -0600

    new commit

---
+++ src/main/java/org/example/segundoparcial/Client.java
@@ -0,0 +1,88 @@
+package org.example.segundoparcial;
+
+//package mx.uv.fiee.iinf.poo.demos.clientsocketv2;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * En esta versión del cliente se permite una conexión uno a uno
+ * recibiendo mensajes del servidor y dando la capacidad al usuario de
+ * responderlos.
+ *
+ * Los puntos principales son, el uso de un ciclo para controlar el flujo
+ * de la aplicación, terminando hasta que se lee de la consola la palabra "stop".
+ *
+ * Para enviar y recibir mensajes es necesario utilizar ambos flujos proporcionados
+ * por el socket, entrada mediante InputStream y salida mediante OutputStream.
+ *
+ * La entrada del usuario es manejado mediante el flujo de entrada de la consola
+ * referenciado por método "in", de la clase System.
+ *
+ */
+public class Client {
+    public static String ADDRESS = "localhost";
+    public static int PORT = 9888;
+
+    public static void main(String [] args) {
+        try {
+            // creamos el socket cliente, utilizando la dirección remota y el mismo puerto de escucha del servidor
+            Socket socket = new Socket (ADDRESS, PORT);
+
+            // creamos los objetos DataInputStream y DataOutputStream, usando el flujo de entrada
+            // y salida del socket
+            DataInputStream din = new DataInputStream (socket.getInputStream ());
+            DataOutputStream dos = new DataOutputStream (socket.getOutputStream ());
+
+            // la entrada del usuario se referencía de la entrada por consola utilizamos un objeto InputStreamReader
+            // para leer los bytes entrantes y los convierta a su representación unicode
+            InputStreamReader in = new InputStreamReader (System.in, StandardCharsets.UTF_8);
+
+            // La clase Reader lee byte a byte e interpreta cada byte de acuerdo a la codificación establecidad,
+            // pero lo que necesitamos es leer bloques (líneas) completas y se almacen en memoria
+            // para ser enviadas, para ellos utilizamos un BufferedReader
+            BufferedReader buff = new BufferedReader (in);
+
+            // Variables de control
+            // line1, almacena la entrada del usuario
+            // line2, lee el mensaje remoto entrante
+            String line1 = "", line2 = "";
+
+            /**
+             * Para realizar lectura y envíos recurrentes, utilizamos un ciclo.
+             *
+             * La aplicación se detiene cuando el usuario teclea "stop"
+             */
+            while (!line1.equals ("stop")) {
+                System.out.print ("Message: ");
+
+                // lee la entrada del usuario mediante la consola del sistema
+                line1 = buff.readLine ();
+
+                // escribe la entradad del usuario en el buffer del flujo de salida
+                dos.writeUTF (line1);
+                dos.flush (); // envía los bytes pendientes alojados en el flujo de salida
+
+                // espera la respuesta del equipo remoto
+                // el flujo de la aplicación se bloquea en este punto, hasta que llega algún dato
+                line2 = din.readUTF ();
+                System.out.println ("Server says: " + line2); // coloca el mensaje entrante en la consola
+
+            }
+
+            // cerramos los flujos de entrada y salida
+            din.close ();
+            dos.close ();
+            socket.close (); // y terminamos la conexión
+
+        } catch (IOException ex) {
+            ex.printStackTrace ();
+        }
+
+    }
+}
---
+++ src/main/java/org/example/segundoparcial/Server.java
@@ -0,0 +1,90 @@
+package org.example.segundoparcial;
+
+//package mx.uv.fiee.iinf.poo.demos.serversocketv2;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * En esta versión del servidor se permite una conexión uno a uno
+ * recibiendo mensajes del cliente y dando la capacidad al usuario de
+ * responderlos.
+ *
+ * Los puntos principales son, el uso de un ciclo para controlar el flujo
+ * de la aplicación, terminando hasta que se lee de la consola la palabra "stop".
+ *
+ * Para enviar y recibir mensajes es necesario utilizar ambos flujos proporcionados
+ * por el socket, entrada mediante InputStream y salida mediante OutputStream.
+ *
+ * La entrada del usuario es manejado mediante el flujo de entrada de la consola
+ * referenciado por método "in", de la clase System.
+ *
+ */
+public class Server {
+    public static int PORT = 9888;
+
+    public static void main(String[] args) {
+        try {
+            // creamos al socket tipo servidor usando el puerto definido
+            ServerSocket server = new ServerSocket (PORT);
+            // esperamos a que se realice una conexión, bloqueando el flujo de la aplicación
+            Socket socket = server.accept ();
+
+            // creamos los objetos DataInputStream y DataOutputStream, usando el flujo de entrada
+            // y salida del socket
+            DataInputStream din = new DataInputStream (socket.getInputStream ());
+            DataOutputStream dos = new DataOutputStream (socket.getOutputStream ());
+
+            // la entrada del usuario se referencía de la entrada por consola
+            // Utilizamos un objeto InputStreamReader para leer los bytes
+            // entrantes y los convierta a su representación unicode
+            InputStreamReader in = new InputStreamReader (System.in, StandardCharsets.UTF_8);
+
+            // La clase Reader lee byte a byte e interpreta cada byte de acuerdo a la codificación establecidad,
+            // pero lo que necesitamos es leer bloques (líneas) completas y se almacen en memoria
+            // para ser enviadas, para ellos utilizamos un BufferedReader
+            BufferedReader buff = new BufferedReader (in);
+
+            // variables de control
+            // line1, lee el mensaje remoto entrante
+            // line2, almacena la entrada del usuario
+            String line1 = "", line2 = "";
+
+            /**
+             * Para realizar lectura y envíos recurrentes, utilizamos un ciclo.
+             *
+             * La aplicación se detiene cuando el usuario teclea "stop"
+             */
+            while (!line1.equals ("stop")) {
+                // lee los bytes entrantes de la conexión como caracteres unicode,
+                // el flujo de la aplicación se detiene esperando la llegada de datos
+                line1 = din.readUTF ();
+                System.out.println ("Client says " + line1);
+
+                System.out.print ("Response: ");
+                // lee la entrada del usuario, el flujo de la aplicación se detiene
+                line2 = buff.readLine ();
+
+                // la entrada del usuario es codificada a caracteres unicode,
+                // y coloca en el buffer de salida de la conexión
+                dos.writeUTF (line2);
+                dos.flush (); // envía los bytes pendientes de salida
+            }
+
+            // cerramos los flujos de entrada y salida
+            din.close ();
+            dos.close ();
+            server.close (); // y finalmente la conexión
+        } catch (IOException ex) {
+            ex.printStackTrace ();
+        }
+
+    }
+
+}
